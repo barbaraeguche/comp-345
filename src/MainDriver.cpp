f9f0aabbb939609ca4f0dfb832eecbcb58e3d88d
sanjitt-k <sanjittk@outlook.com>
2025-11-11 18:20:22 -0500

    Sanjitt/startupphase (2) (#24)
    * bare bones
    
    * remove command from game-engine
    
    * remove command from game-engine
    
    * finalize command processing
    
    * finished command processing
    
    * haven't tested reading commands from file yet
    
    * added some dummy command text files
    
    * changed naming for command conflict in gameengine and used command processing for a2
    
    * added startup phase for file and console
    
    * Implement test for startupPhase in GameEngine
    
    Added test function for startupPhase in GameEngine.
    
    * Add startup phase testing function
    
    Added a new test function for the startup phase.
    
    * Update command validation for game states
    
    added loadmap for start / gamestart for addplayers and help for any state
    
    ---------
    
    Co-authored-by: barbaraeguche <barbaraeguche@icloud.com>

--- src/MainDriver.cpp
+++ src/MainDriver.cpp
@@ -5,7 +5,9 @@ void testPlayers();
 void testOrdersLists(); 
 void testOrderExecution(); 
 void testCards(); 
-void testGameStates(); 
+void testGameStates();
+void testCommandProcessor();
+void testStartupPhase();
 
 /**
  * The Main driver function that calls all test functions for each part
@@ -48,6 +50,18 @@ int main() {
     std::cout << "PART 5: GAME ENGINE TESTING" << std::endl;
     std::cout << std::string(50, '=') << std::endl;
     testGameStates();
+
+    // Part 6: Command Processor Testing
+    std::cout << "\n" << std::string(50, '=') << std::endl;
+    std::cout << "PART 6: COMMAND PROCESSOR TESTING" << std::endl;
+    std::cout << std::string(50, '=') << std::endl;
+    testCommandProcessor();
+
+    // Part 7: startup phase Testing
+    std::cout << "\n" << std::string(50, '=') << std::endl;
+    std::cout << "PART 6: STARTUP PHASE TESTING" << std::endl;
+    std::cout << std::string(50, '=') << std::endl;
+    testStartupPhase();
 
     std::cout << "\n" << std::string(50, '=') << std::endl;
     std::cout << "ALL TESTS COMPLETED SUCCESSFULLY" << std::endl;
